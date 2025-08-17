71667c7eaa44ffab22c7807913f0acb591eb9903
DivyaGaurav21 <divyag.ug16.ee@nitp.ac.in>
2025-08-17 18:26:31 +0530

    added cateogary wise listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ProductCarousel from './component/ProductCarousel'
 import ProductList from './component/ProductList'
+import CateogaryItem from './component/CateogaryItem'
 
 const page = () => {
   return (
     <div>
       <ProductCarousel/>
       <ProductList/>
+      <CateogaryItem/>
     </div>
   )
 }
